5dcf0a5c995ccd2f940315acd566b130113f2ca8
zikowashere <zakdaouma@gmail.com>
2019-03-17 11:11:10 +0100

    app-fitness

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ const path=require('path');
 app.use(express.static(__dirname+'/dist'));
 app.listen(process.env.PORT||8080);
 app.get('/*',function(req,res){
-    res.sendfile(path.join(__dirname+'/dist/project-test/index.html'));
+    res.sendfile(path.join(__dirname+'/dist/index.html'));
 })
-console.log('console listnening')
+console.log('console listnening');
